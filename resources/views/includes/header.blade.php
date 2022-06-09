f38d18eac74c6362225fbfbdd7fb4bd9ebdee18c
Rico Pfister <contact.rico.pfister@gmail.com>
2022-06-09 10:51:00 +0200

    css animation

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,14 @@
+
+<link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+{{-- <link rel="stylesheet" href="{{ mix('/css/header.css') }}"> --}}
+
+<div class="header">
+
+    {{-- <img class="logo" src="img/logo.png"> --}}
+
+    <div id="headerWrapper">
+        <h1 class="bannerPart1 banner">RetroGames</h1>
+        <h3 class="bannerPart2 banner">TopSeller</h3>
+      </div>
+
+</div>
