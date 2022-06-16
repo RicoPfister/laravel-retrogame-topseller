645e0c5e687e17f520cfc77024c97543caf60fa5
beniwein <beni_weinmann@hotmail.com>
2022-06-16 10:06:21 +0200

    index-comment

--- resources/views/comment.blade.php
+++ resources/views/comment.blade.php
@@ -1,11 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Game-Index</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link href="{{ asset('/css/rating_search.css') }}" rel="stylesheet">
+  <script src="{{ asset('/js/sortresult.js') }}"></script>
+
+</head>
+
 <div class="comment border border-1 border-dark d-flex justify-content-center py-4 mb-5">
 
     <div>
 
         <h1><b>Comments</b></h1>
-        <div><b>[show comments module]</b></div>
+        @include('feedback')
         <br>
-        <div><b>[write comment module] </b></div>
+        <div><b>[show comments module] </b></div>
 
     </div>
 
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -56,7 +56,8 @@
                 </div>
 
                 {{-- separator --}}
-                <div class="row mb-3"></div>
+                <div class="row mb-3 d-flex justify-content-end"></div>
+                @include('searchresult')
 
                 {{-- comment moduele --}}
                 <div class="row">
@@ -75,6 +76,7 @@
         </div>
 
     </div>
+
 
     <div class="footer"><b>Updated: {{date('d.m.Y')}}</b></div>
 </body>
