5997bc83ee30e4f4b88fa5c5faebaffd389a12a8
Rico Pfister <contact.rico.pfister@gmail.com>
2022-06-16 08:53:30 +0200

    add next/previous button; change cover names

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Collection;
 
 class CollectionController extends Controller
+
 {
     public function gameIndex(Request $request) {
 
@@ -15,9 +16,36 @@ class CollectionController extends Controller
 
     // $gameIndex = Collection::all()->sortByDesc('created_at');
 
-    $gameIndex = Collection::all()->random();
+    // if(isset($request->previous)){
+    //     $entry = $request->session()->get('entry');
+    //     $gameIndex = Collection::find($entry-1);
+    //     $request->session()->put('entry',$entry-1);
+    // }
 
+    // elseif (isset($request->next)){
+    //     $entry = $request->session()->get('entry');
+    //     $gameIndex = Collection::find($entry+1);
+    //     $request->session()->put('entry',$entry+1);
+    // }
+
+    if ($request->session()->has('previous')){
+        $entry = $request->session()->get('entry');
+        $gameIndex = Collection::find($entry);
+        $request->session()->forget('previous');
+    }
+
+    elseif ($request->session()->has('next')){
+        $entry = $request->session()->get('entry');
+        $gameIndex = Collection::find($entry);
+        $request->session()->forget('next');
+    }
+
+    else {
+    $gameIndex = Collection::all()->random();
     $request->session()->put('entry',$gameIndex['id']);
+    }
+
+    // $request->session()->put('entry',$entry+1);
 
     // This line would output the messages in the UI/Browser // and stop the script execution.
     // good for debugging ;o)
@@ -26,7 +54,12 @@ class CollectionController extends Controller
     // here the blade template engine works its magic again // through which we cann pass the $messages array to the view. // we can pass it as an optional second paramter (
     // associative array)
 
-    return view('layouts/default', ['collection' => $gameIndex]);
+    // $request->flash();
+
+    return view('layouts/default', ['collection' => $gameIndex, ]);
+    // return back()->with(['collection' => $gameIndex]);
+    // return redirect('/')->with(['collection' => $gameIndex]);
+    // return back()->withInput();
     }
 
     public function gameIndexPrevious(Request $request) {
@@ -38,8 +71,17 @@ class CollectionController extends Controller
     // $gameIndex2 = Collection::all()->sortByDesc('created_at');
     // $request->session()->put('entry',1);
     $entry = $request->session()->get('entry');
-    $gameIndex = Collection::find($entry-1);
-    $request->session()->put('entry',$entry-1);
+
+    if($request->session()->get('entry') == 1) {
+        $request->session()->put('entry',$gameIndex = Collection::all()->count());
+    }
+
+    else {
+        // $gameIndex = Collection::find($entry-1);
+        $request->session()->put('entry',$entry-1);
+    }
+
+    $request->session()->put('previous',1);
 
     // This line would output the messages in the UI/Browser // and stop the script execution.
     // good for debugging ;o)
@@ -48,7 +90,8 @@ class CollectionController extends Controller
     // here the blade template engine works its magic again // through which we cann pass the $messages array to the view. // we can pass it as an optional second paramter (
     // associative array)
 
-    return view('layouts/default', ['collection' => $gameIndex]);
+    return redirect('/');
+    // return view('layouts/default', ['collection' => $gameIndex]);
     }
 
     public function gameIndexNext(Request $request) {
@@ -59,8 +102,17 @@ class CollectionController extends Controller
 
     // $gameIndex = Collection::all()->sortByDesc('created_at');
     $entry = $request->session()->get('entry');
-    $gameIndex = Collection::find($entry+1);
-    $request->session()->put('entry',$entry+1);
+
+    if($request->session()->get('entry') == Collection::all()->count()) {
+        $request->session()->put('entry',$gameIndex = 1);
+    }
+
+    else {
+        // $gameIndex = Collection::find($entry+1);
+        $request->session()->put('entry',$entry+1);
+    }
+
+    $request->session()->put('next',1);
 
     // This line would output the messages in the UI/Browser // and stop the script execution.
     // good for debugging ;o)
@@ -69,6 +121,7 @@ class CollectionController extends Controller
     // here the blade template engine works its magic again // through which we cann pass the $messages array to the view. // we can pass it as an optional second paramter (
     // associative array)
 
-    return view('layouts/default', ['collection' => $gameIndex]);
+    // return view('layouts/default', ['collection' => $gameIndex]);
+    return redirect('/');
     }
 }
